c858e26ea895d476dd2a9132a8d6d7b0f800e1bc
Bryan Nilsen <bryannilsen@gmail.com>
2021-02-08 08:32:18 -0600

    updated styling

--- scripts/menu/MenuItem.js
+++ scripts/menu/MenuItem.js
@@ -1,7 +1,7 @@
 export const MenuItem = (menuObj) => {
     // return HTML representation of single menu object
     return `
-    <div>
+    <div class="mar-b">
         <h3>${menuObj.name} - ${menuObj.price}</h3>
         <p>${menuObj.description}</p>
     </div>
